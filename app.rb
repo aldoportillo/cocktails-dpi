6b29185b4a6e6a1ccafbf7e27c0028844ea3e539
Aldo Portillo <94483081+aldoportillo@users.noreply.github.com>
2023-07-28 23:32:52 +0000

    Create a request to find cocktails

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
+require "json"
 
 get("/") do
   "
@@ -7,3 +9,18 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+get("/cocktail_search"){
+  
+  erb(:cocktail_search)
+}
+
+post("/cocktail_result"){
+  @cocktail_name = params.fetch("cocktail_name")
+
+  API = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=#{CGI.escape(@cocktail_name)}"
+
+  req = HTTP.get(API)
+  @res = JSON.parse(req)
+
+  erb(:cocktail_result)
+}
