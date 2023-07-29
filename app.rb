d22f9c71360a99340f896c790047826897e1678b
Aldo Portillo <94483081+aldoportillo@users.noreply.github.com>
2023-07-29 04:43:52 +0000

    Create route to get drink
    and pass down data

--- app.rb
+++ app.rb
@@ -22,5 +22,23 @@ post("/cocktail_result"){
   req = HTTP.get(API)
   @res = JSON.parse(req)
 
+  @cocktails = @res.fetch("drinks")
+
   erb(:cocktail_result)
+}
+
+get("/cocktail/:id"){
+  @id = params.fetch("id")
+  API = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=#{@id}"
+
+  req = HTTP.get(API)
+  @res = JSON.parse(req).dig('drinks', 0)
+
+  @name, @glass, @image, @instructions = @res.dig("strDrink"), @res.dig("strGlass"), @res.dig("strDrinkThumb"), @res.dig("strInstructions")
+
+  #Missing ingredients
+  #strIngredient4
+  #strMeasure2
+
+  erb(:cocktail)
 }
